53867be45b51163394b6a0312a5977b928714490
subha <subha@DESKTOP-CEKL42O>
2021-10-19 15:38:43 +0530

    commiting now

--- Subha/src/main/java/Mygit/Github.java
+++ Subha/src/main/java/Mygit/Github.java
@@ -1,6 +1,9 @@
 package Mygit;
 
 public class Github {
+	public void creayeCustomer() {
+		System.out.println("new customer created");
+	}
 	public static void main(String[] args) {
 		System.out.println("New repo has been deleted"); 
 	}
