0f2e712431abf9c7c2d6c193c6487e9bc9d1033e
subha <subha@DESKTOP-CEKL42O>
2021-10-19 15:34:06 +0530

    commit

--- Subha/src/main/java/Mygit/Github.java
+++ Subha/src/main/java/Mygit/Github.java
@@ -2,7 +2,7 @@ package Mygit;
 
 public class Github {
 	public static void main(String[] args) {
-		System.out.println("New repo has been created");
+		System.out.println("New repo has been deleted"); 
 	}
 
 }
